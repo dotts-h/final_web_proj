e7e3d10ae915474fa23ee92d20adfdb54c5ebfd9
Horia C. Nuță <n.horia.c@hotmail.com>
2021-06-22 16:21:55 +0300

    t

--- public/index.php
+++ public/index.php
@@ -1,30 +1,31 @@
 <?php
 
-try {
-	include __DIR__ . '/../includes/autoload.php';
-	// include __DIR__ . '/../includes/DatabaseConnection.php';
-	// include __DIR__ . '/../classes/DatabaseTable.php';
-	// include __DIR__ . '/../classes/controllers/JokeController.php';
-	// include __DIR__ . '/../classes/EntryPoint.php';
-	// include __DIR__ . '/../classes/IjdbRoutes.php';
+// try {
+// 	include __DIR__ . '/../includes/autoload.php';
+// 	// include __DIR__ . '/../includes/DatabaseConnection.php';
+// 	// include __DIR__ . '/../classes/DatabaseTable.php';
+// 	// include __DIR__ . '/../classes/controllers/JokeController.php';
+// 	// include __DIR__ . '/../classes/EntryPoint.php';
+// 	// include __DIR__ . '/../classes/IjdbRoutes.php';
 
-	// $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
-	// $authorsTable = new DatabaseTable($pdo, 'author', 'id');
+// 	// $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
+// 	// $authorsTable = new DatabaseTable($pdo, 'author', 'id');
 
-	// $route = $_GET['route'] ?? 'joke/home';
-	$route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
-	// print 'route' . $route;
+// 	// $route = $_GET['route'] ?? 'joke/home';
+// 	$route = ltrim(strtok($_SERVER['REQUEST_URI'], '?'), '/');
+// 	// print 'route' . $route;
 
 
-	$entryPoint = new \Ninja\EntryPoint($route, $_SERVER['REQUEST_METHOD'], new \Ijdb\IjdbRoutes());
-	$entryPoint->run();
-} catch (PDOException $e) {
-	$title = 'An error has occurred';
+// 	$entryPoint = new \Ninja\EntryPoint($route, $_SERVER['REQUEST_METHOD'], new \Ijdb\IjdbRoutes());
+// 	$entryPoint->run();
+// } catch (PDOException $e) {
+// 	$title = 'An error has occurred';
 
-	$output = 'Database error: ' .
-		$e->getMessage() . ' in ' .
-		$e->getFile() . ':' .
-		$e->getLine();
+// 	$output = 'Database error: ' .
+// 		$e->getMessage() . ' in ' .
+// 		$e->getFile() . ':' .
+// 		$e->getLine();
 
-	include __DIR__ . '/../templates/layout.html.php';
-}
+// 	include __DIR__ . '/../templates/layout.html.php';
+// }
+echo "<h1>test</h1>";
