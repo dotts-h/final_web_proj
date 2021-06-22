ff856bf14349e58dc83b591ffa64223164412d92
Horia C. Nuță <n.horia.c@hotmail.com>
2021-06-22 14:20:56 +0300

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b268b5b26a5b48ef0d22db0e6a221bb4299b8e6d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b268b5b26a5b48ef0d22db0e6a221bb4299b8e6d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
