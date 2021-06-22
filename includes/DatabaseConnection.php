f8b27f78ffb96b84b61cecb1bc0ea2ba309c4800
Horia C. Nuță <n.horia.c@hotmail.com>
2021-06-22 14:57:19 +0300

    t

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,7 +1,7 @@
 <?php
 $pdo = new PDO(
-	'mysql:host=localhost;dbname=ijdb;charset=utf8',
-	'ijdbuser',
-	'Tester123'
+	'mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_a742c7cd17ba7cb;charset=utf8',
+	'b90e8a074246f2',
+	'0de66ca8'
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
