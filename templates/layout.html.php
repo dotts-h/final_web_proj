313061f09b775235dc25a951ab3b990e0316445c
Horia C. Nuță <n.horia.c@hotmail.com>
2021-06-22 17:36:33 +0300

    t

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/../../css/jokes.css">
+    <link rel="stylesheet" href=<?= __DIR__ . '/../../css/jokes.css' ?>>
     <title><?= $title ?></title>
 </head>
 
